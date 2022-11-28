9a3ad84c5312bae5c23d58156dc9fe725cd54f5c
atienoobwanda <atienoobwanda@gmail.com>
2022-11-28 12:08:35 +0300

    adding the url for watch

--- Store/urls.py
+++ Store/urls.py
@@ -8,4 +8,6 @@ app_name = 'Store'
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('zenith/watches/<slug:slug>', views.watch_detail, name='watch_detail'),
+    
 ]
