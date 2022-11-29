93b38b13d6b043deb69e2e29198ee4f6f7f20184
atienoobwanda <atienoobwanda@gmail.com>
2022-11-29 15:45:22 +0300

    adding the delete and update cart views

--- Cart/views.py
+++ Cart/views.py
@@ -24,3 +24,29 @@ def cart_add(request):
         response = JsonResponse({'qty': cartqty})
         # response = JsonResponse({'test':'data'})
         return response
+
+
+def cart_delete(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        watch_id = int(request.POST.get('watchid'))
+        cart.delete(watch=watch_id)
+
+        cartqty = cart.__len__()
+        carttotal = cart.get_total_price()
+        # response = JsonResponse({'Success': True})
+        response = JsonResponse({'qty': cartqty, 'subtotal': carttotal})
+        return response
+
+def cart_update(request): # update
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        watch_id = int(request.POST.get('watchid'))
+        watch_qty = int(request.POST.get('watchqty'))
+        cart.update(watch=watch_id, qty=watch_qty)
+
+        cartqty = cart.__len__()
+        carttotal = cart.get_total_price()
+        # newPrice = item.total_price()
+        response = JsonResponse({'qty': cartqty, 'subtotal': carttotal})
+        return response
