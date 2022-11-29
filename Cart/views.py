7a07b7c32c53e8211c8bfe3bc3e3a62f938df050
atienoobwanda <atienoobwanda@gmail.com>
2022-11-29 09:31:05 +0300

    adding the view for adding items to the cart

--- Cart/views.py
+++ Cart/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
 
+from Store.models import Watch
+from .cart import Cart
 
 
 def cart_summary(request):
-    return render(request, 'Cart/cart.html')
+    cart = Cart(request)
+    return render(request, 'Cart/cart.html', {'cart': cart})
+
+def cart_add(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        watch_id = int(request.POST.get('watchid'))
+        watch_qty = int(request.POST.get('watchqty'))
+        watch = get_object_or_404(Watch, id=watch_id)
+
+        cart.add(watch=watch, qty=watch_qty)
+        cartqty = cart.__len__()
+
+        response = JsonResponse({'qty' : cartqty})
+        return response
