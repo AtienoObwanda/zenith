640e49c47c8118527739eacf063a2917d26ff4f8
atienoobwanda <atienoobwanda@gmail.com>
2022-12-09 07:39:01 +0300

    db

--- Core/settings.py
+++ Core/settings.py
@@ -73,10 +73,10 @@ WSGI_APPLICATION = 'Core.wsgi.application'
 # Database
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
