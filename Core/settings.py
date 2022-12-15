a3c3eadb81b8107caa6df4c9ad793f254b0c8c23
atienoobwanda <atienoobwanda@gmail.com>
2022-12-15 13:58:00 +0300

    removing vercel configurations

--- Core/settings.py
+++ Core/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 
 STATICFILES_DIRS = (
@@ -153,4 +153,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
