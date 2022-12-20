920d4833736248d0c348b1c18911031823a71f69
atienoobwanda <atienoobwanda@gmail.com>
2022-12-20 03:15:20 +0300

    sendgrid config

--- Account/views.py
+++ Account/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, redirect
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.sites.shortcuts import get_current_site
@@ -10,14 +13,85 @@ from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
 # from orders.views import user_orders
-
 from .models import UserBase
-
 from .forms import RegistrationForm, UserLoginForm, PwdResetForm, PwdResetConfirmForm, UserEditForm
 from .token import account_activation_token
 
+# sendgrid Test config
 
+# message = Mail(
+#     from_email='pm@opinion-space.com',
+#     to_emails='atienoobwanda@gmail.com',
+#     subject='Sending with Twilio SendGrid is Fun',
+#     html_content='<strong>and easy to do anywhere, even with Python</strong>')
+# try:
+#     sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+#     response = sg.send(message)
+#     print(response.status_code)
+#     print(response.body)
+#     print(response.headers)
+# except Exception as e:
+#     print(e.message)
+    
+    
+    
 def account_register(request):
+    if request.user.is_authenticated:
+        return redirect('Account:dashboard')
+        
+    if request.method == 'POST':
+        registerForm = RegistrationForm(request.POST)
+        if registerForm.is_valid():
+            user = registerForm.save(commit=False)
+            user.email = registerForm.cleaned_data['email']
+            user.set_password(registerForm.cleaned_data['password'])
+            user.is_active = False
+            # user.save()
+            current_site = get_current_site(request)
+            subject = 'Activate your Account'
+            # message = render_to_string('Account/account_activation_email.html',{
+            #     'user': user,
+            #     'domain': current_site.domain,
+            #     'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            #     'token': account_activation_token.make_token(user),
+            #      })
+            # user.email_user(subject=subject, message=message)
+            uName = registerForm.cleaned_data['user_name']
+            user_email = user.email
+            message = Mail(
+                from_email='pm@opinion-space.com',
+                to_emails=[user_email],
+                subject='Welcome To Zenith!',
+                html_content='Hey, Your Zenith user Account has been created successfully...'
+            )
+            print(user_email)
+            # message.dynamic_template_data = {
+            #     'user': user,
+            #     'domain': current_site.domain,
+            #     'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            #     'token': account_activation_token.make_token(user),
+            #     'user_name': Uname,
+
+            #         }
+            # message.template_id =  'd-1f61c397b2334ce5b4628a9cbf19c437'
+            user.save()
+
+            try:
+                sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+                response = sg.send(message)
+                print(response.status_code)
+                print(response.body)
+                print(response.headers)
+            except Exception as e:
+                print(e.message)
+            return HttpResponse('User registered successfully! Kindly check email for activation link!') # Create a page for this
+        
+    else:
+        registerForm = RegistrationForm()
+    return render(request, 'Account/register.html', {'form': registerForm})
+
+
+def accout_register(request):
     if request.user.is_authenticated:
         return redirect('Account:dashboard')
         
@@ -39,18 +113,7 @@ def account_register(request):
                  })
             user.email_user(subject=subject, message=message)
             return HttpResponse('User registered successfully! Kindly check email for activation link!') # Create a page for this
-            # def send_activation_email(self, request, user):
-            #     current_site = get_current_site(request)
-            #     subject = 'Activate Your Account'
-            #     message = render_to_string('Account/account_activation_email.html',{
-            #         'user': user,
-            #         'domain': current_site.domain,
-            #         'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-            #         'token': account_activation_token.make_token(user),
-            #         })
-            #     user.email_user(subject, message, html_message=message)
-            #     print(settings.DEFAULT_FROM_EMAIL)
-            # return HttpResponse('User registered successfully! Kindly check email for activation link!') # Create a page for this            
+        
     else:
         registerForm = RegistrationForm()
     return render(request, 'Account/register.html', {'form': registerForm})
